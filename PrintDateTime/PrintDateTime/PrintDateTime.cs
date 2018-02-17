676e5517fc87c98d883b0ded134ba875d3393f06
SnipeHub <v-macar2@microsoft.com>
2018-02-17 11:56:04 +1100

    PrintDateTime solution added

---
+++ PrintDateTime/PrintDateTime/PrintDateTime.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace PrintDateTime
+{
+    class PrintDateTime
+    {
+        static void Main(string[] args)
+        {
+            DateTime now = DateTime.Now;
+            Console.WriteLine(now);
+            Console.ReadLine();
+        }
+    }
+}

d83cd9c669df17ff8d2cb175cf6939bb86976d60
SnipeHub <v-macar2@microsoft.com>
2018-02-18 20:59:08 +1100

    chp 2

---
+++ Strings/Strings/StringVariable.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Strings
+{
+    class StringVariable
+    {
+        static void Main(string[] args)
+        {
+
+            // variables (String variables)
+            string firstName = "John";
+            string lastName = "Smith";
+            // Added extra string variable
+            string Title = "Mr";
+            string fullName = Title + " " + firstName + " " + lastName;
+
+            // print to console
+            Console.WriteLine("Hello, " + firstName + "!");
+            Console.WriteLine("Your full name is " + fullName + ".");
+
+            //// Console output:
+            // Hello, John!
+            // Your full name is Mr John Smith.
+        }
+    }
+}

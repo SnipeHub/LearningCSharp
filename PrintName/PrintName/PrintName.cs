b74a64e3cb371da7b90149a904e5934f4c889b69
SnipeHub <v-macar2@microsoft.com>
2018-02-20 21:11:23 +1100

    PrintName changes

--- PrintName/PrintName/PrintName.cs
+++ PrintName/PrintName/PrintName.cs
@@ -7,6 +7,7 @@ namespace PrintName
         static void Main(string[] args)
         {
             Console.WriteLine("SnipeHub is coding, yahhoooo!");
+            Console.ReadLine(); // Used so that the console needs to be closed from the X button, pressing any key does not work
         }
     }
 }

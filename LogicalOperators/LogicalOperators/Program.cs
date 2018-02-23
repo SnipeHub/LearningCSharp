27a3372f9d665fe019e219bda2ae7db56fcc99a6
SnipeHub <v-macar2@microsoft.com>
2018-02-23 16:49:45 +1100

    update

---
+++ LogicalOperators/LogicalOperators/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalOperators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool a = true;
+            bool b = false;
+            Console.WriteLine(a && b);              // false
+            Console.WriteLine(a || b);              // true
+            Console.WriteLine(!b);                  // true
+            Console.WriteLine(b || true);           // true
+            Console.WriteLine((5 > 7) ^ (a == b));  // false
+
+            // Laws of De Morgan
+            // !(a && b) == (!a || !b)
+            // !(a || b) == (!a && !b)
+        }
+    }
+}

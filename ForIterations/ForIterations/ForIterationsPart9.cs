ab64846255b5af588df37265a2bccff0db3705b3
SnipeHub <v-macar2@microsoft.com>
2018-02-25 20:51:07 +1100

    uploaded parts 8 & 9 solutions, made some small improvements to my timesheet calculator app

---
+++ ForIterations/ForIterations/ForIterationsPart9.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForIterations
+{
+    class ForIterationsPart9
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++) // i++ - This means you are incrementing the value of 'i' by 1. This will increment by 1 until it reaches 10
+            {
+                //Console.WriteLine(i.ToString());
+
+            if (i == 7)
+                {
+                    Console.WriteLine("Found seven!");
+                    break; // used to break out of the code block
+                }
+            }
+
+            for (int myValue = 0; myValue < 12; myValue++) // This line was created by first typing the "for" statement, then hitting the TAB key twice to auto type the rest ready for editng
+            {
+                Console.WriteLine(myValue);
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
--- MathPractice/MathPractice/MathPractice.cs
+++ MathPractice/MathPractice/MathPractice.cs
@@ -25,25 +25,27 @@ namespace NumbersPractice
             Console.WriteLine("Monday Total: " + dayTotalOne + " - " + "Admin: " + adminOne);
             */
 
-            double Monday = 0.5 + 3.0;
-            double adminMon = 7.5 - Monday;
-            Console.WriteLine("Monday 19.02.2018 is: " + Monday + " - "  + "Admin is: " + adminMon);
+            double Mon, Tue, Wed, Thu, Fri, Sat, Sun; // Fixing up further by declaring all variables up here and shortening it
 
-            double Tuesday = 0.7 + 2.9;
-            double adminTue = 7.5 - Tuesday;
-            Console.WriteLine("Tuesday 20.02 is: " + Tuesday + " - " + "Admin is: " + adminTue);
+            Mon = 0.5 + 3.0; // Edit these values for each day
+            double adminMon = 7.5 - Mon;
+            Console.WriteLine("Monday 19.02.2018 is: " + Mon + " - "  + "Admin is: " + adminMon);
 
-            double Wednesday = 1.7 + 2.2;
-            double adminWed = 7.5 - Wednesday;
-            Console.WriteLine("Wednesday 21.02 is: " + Wednesday + " - " + "Admin is: " + adminWed);
+            Tue = 0.7 + 2.9;
+            double adminTue = 7.5 - Tue;
+            Console.WriteLine("Tuesday 20.02 is: " + Tue + " - " + "Admin is: " + adminTue);
 
-            double Thursday = 1.8 + 2.0;
-            double adminThur = 7.5 - Thursday;
-            Console.WriteLine("Thursday 22.02 is: " + Thursday + " - " + "Admin is: " + adminThur);
+            Wed = 1.7 + 2.2;
+            double adminWed = 7.5 - Wed;
+            Console.WriteLine("Wednesday 21.02 is: " + Wed + " - " + "Admin is: " + adminWed);
 
-            double Friday = 1.0 + 2.8;
-            double adminFri = 7.5 - Friday;
-            Console.WriteLine("Friday 23.02 is: " + Friday + " - " + "Admin is: " + adminFri);
+            Thu = 1.8 + 2.0;
+            double adminThur = 7.5 - Thu;
+            Console.WriteLine("Thursday 22.02 is: " + Thu + " - " + "Admin is: " + adminThur);
+
+            Fri = 1.0 + 2.8;
+            double adminFri = 7.5 - Fri;
+            Console.WriteLine("Friday 23.02 is: " + Fri + " - " + "Admin is: " + adminFri);
 
             Console.ReadLine();
 

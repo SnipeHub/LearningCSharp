8d4b3b985b0aef9a53dab876748b794cab8765a5
SnipeHub <v-macar2@microsoft.com>
2018-03-05 15:54:47 +1100

    Minor visability changes "\n"

--- MathPractice/MathPractice/MathPractice.cs
+++ MathPractice/MathPractice/MathPractice.cs
@@ -25,27 +25,30 @@ namespace NumbersPractice
             Console.WriteLine("Monday Total: " + dayTotalOne + " - " + "Admin: " + adminOne);
             */
 
+            DateTime now = DateTime.Now;
+            Console.WriteLine("Current Date and Time: " + now + "\n");
+
             double Mon, Tue, Wed, Thu, Fri, Sat, Sun; // Fixing up further by declaring all variables up here and shortening it
 
             Mon = 0.9 + 1.8; // Edit these values for each day
             double adminMon = 7.5 - Mon;
-            Console.WriteLine("Monday is: " + Mon + " - "  + "Admin is: " + adminMon);
+            Console.WriteLine("Monday is: " + Mon + " - "  + "Admin is: " + adminMon + "\n");
 
             Tue = 2.0 + 3.4;
             double adminTue = 7.5 - Tue;
-            Console.WriteLine("Tuesday is: " + Tue + " - " + "Admin is: " + adminTue);
+            Console.WriteLine("Tuesday is: " + Tue + " - " + "Admin is: " + adminTue + "\n");
 
             Wed = 1.4 + 3.3;
             double adminWed = 7.5 - Wed;
-            Console.WriteLine("Wednesday is: " + Wed + " - " + "Admin is: " + adminWed);
+            Console.WriteLine("Wednesday is: " + Wed + " - " + "Admin is: " + adminWed + "\n");
 
             Thu = 1.2 + 3.8;
             double adminThur = 7.5 - Thu;
-            Console.WriteLine("Thursday 01.03.2018 is: " + Thu + " - " + "Admin is: " + adminThur);
+            Console.WriteLine("Thursday 01.03.2018 is: " + Thu + " - " + "Admin is: " + adminThur + "\n");
             /*
             Fri = 1.0 + 2.8;
             double adminFri = 7.5 - Fri;
-            Console.WriteLine("Friday 23.02 is: " + Fri + " - " + "Admin is: " + adminFri);                 
+            Console.WriteLine("Friday 23.02 is: " + Fri + " - " + "Admin is: " + adminFri + "\n");                 
             */
             Console.ReadLine();
 

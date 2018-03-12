d48f0ff4709e6159b12b3235b3e8e3239c2abdbb
SnipeHub <v-macar2@microsoft.com>
2018-03-12 17:51:51 +1100

    Part 18 Namespace referencing Assemblies

---
+++ NamespacesAndReferencingAssemblies/NamespacesAndReferencingAssemblies/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tabor;
+
+namespace NamespacesAndReferencingAssemblies // Part 18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Bob bob = new Bob();
+
+            string html = bob.Lookup("http://www.google.com");
+
+            Console.WriteLine(html);
+            Console.ReadLine();
+        }
+    }
+}

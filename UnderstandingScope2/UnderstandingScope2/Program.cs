5ab55d5bccf562a8b4ad46902c52447df989946b
SnipeHub <v-macar2@microsoft.com>
2018-03-13 17:54:26 +1100

    Part 19 Understading Scope, 2 parts, difference between private and public

---
+++ UnderstandingScope2/UnderstandingScope2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnderstandingScope2 // Part 19 
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car();
+
+            car.DoSomething(); // unable to see the private string, can only call from the public
+
+
+        }
+    }
+
+    class Car
+    {
+        public void DoSomething()
+        {
+            Console.WriteLine(helperMethod());
+        }
+
+        private string helperMethod()
+        {
+            return "Hello World!"; // used as an example, not something you would do to call hello world.
+        }
+    }
+}
---
+++ UndertsandingScope/UndertsandingScope/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UndertstandingScope // Part 19
+{
+    class Program
+    {
+        private static string k = ""; // k defined outside. A private method can only be called from inside the private, public, from outside
+
+        static void Main(string[] args)
+        {
+            string j = "";
+
+            for (int i = 0; i < 10; i++)
+            {
+                j = i.ToString();
+                k = i.ToString();
+                Console.WriteLine(i);
+
+                if (i == 9)
+                {
+                    string l = i.ToString();
+                }
+                //Console.WriteLine("l: " + l); // Wont work outside of the if block
+            }
+
+            Console.WriteLine("Outside of the for: " + j);
+            //Console.WriteLine("k: " + k);
+            helperMethod();
+        }
+
+        static void helperMethod()
+        {
+            Console.WriteLine("k from the helpermethod: " + k); // works as it was defined outside
+        }
+    }
+}

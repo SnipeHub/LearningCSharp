e3291a4cb7b6e53dafa7c2ddb10161f7954d1c06
SnipeHub <v-macar2@microsoft.com>
2018-03-14 20:21:20 +1100

    Part 20 Enumerations and switch decision statement

---
+++ UnderstandingEnumerations/UnderstandingEnumerations/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnderstandingEnumerations // Part 20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+
+            //Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Type in a superhero's name to see his nickname: ");
+            string userValue = Console.ReadLine();
+
+            SuperHero myValue;
+
+            if (Enum.TryParse<SuperHero>(userValue, true, out myValue)) // true ignores capital letters
+            {
+
+                switch (myValue) // type switch, then press tab twice. Enter your variable name into the bracket, then press enter twice and it will auto create the rest. Make sure you have created your enum first.
+                {
+                    case SuperHero.Batman:
+                        Console.WriteLine("Caped crusader");
+                        break;
+                    case SuperHero.Superman:
+                        Console.WriteLine("Man of Steel");
+                        break;
+                    case SuperHero.GreenLantern:
+                        Console.WriteLine("Emerald Knight");
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            else
+            {
+                Console.WriteLine("Does not Compute");
+            }
+
+            Console.ReadLine();
+        }
+    }
+
+    enum SuperHero
+    {
+        Batman,
+        Superman,
+        GreenLantern
+    }
+}

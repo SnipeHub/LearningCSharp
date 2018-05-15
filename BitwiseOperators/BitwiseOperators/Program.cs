5f64445fe2eac36bb3fdf5177a29d0d8ca3748a0
SnipeHub <itsniper@openmailbox.org>
2018-05-15 16:16:54 +1000

    Bitwise operators chp3

---
+++ BitwiseOperators/BitwiseOperators/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BitwiseOperators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            byte a = 3;             // 0000 0011 = 3
+            byte b = 5;             // 0000 0101 = 5
+
+            Console.WriteLine(a | b);
+            Console.WriteLine(a & b);
+        }
+    }
+}

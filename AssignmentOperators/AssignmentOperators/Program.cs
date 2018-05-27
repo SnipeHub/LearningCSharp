abac9e39bc1322959737c944a42db602792c0d5b
SnipeHub <v-macar2@microsoft.com>
2018-05-27 15:35:18 +1000

    Assignment Operators

---
+++ AssignmentOperators/AssignmentOperators/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssignmentOperators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /* The operator for assigning value to a variable is "=" */
+
+            int x = 6;
+            string hellostring = "Hello string.";
+            int y = x;
+
+            Console.WriteLine(hellostring);
+
+        }
+    }
+}
